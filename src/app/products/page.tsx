b5780b17c765ec8e5c655738ec8b16656c701acd
Your NameOmar <omvxvr@gmail.com>
2026-04-02 15:45:18 +0200

    admin panel is next

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,17 @@
-import ProductList from "@/components/ProductList"
-import { promises } from "dns"
+import ProductList from "@/components/ProductList";
+import { promises } from "dns";
 
-const ProductsPage = async ({searchParams} : {searchParams: Promise <{category: string}>}) => {
-    const category = (await searchParams).category
+const ProductsPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ category: string }>;
+}) => {
+  const category = (await searchParams).category;
   return (
-    <div className='mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl'>
-        <ProductList category={category} params="products"/>
+    <div className="mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
+      <ProductList category={category} params="products" />
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
--- src/components/ShoppingCartIcon.tsx
+++ src/components/ShoppingCartIcon.tsx
@@ -1,21 +1,21 @@
-"use client"
+"use client";
 
-import useCartStore from "@/stores/cartStore"
-import { ShoppingCart } from "lucide-react"
-import Link from "next/link"
+import useCartStore from "@/stores/cartStore";
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
 const ShoppingCartIcon = () => {
-  const {cart, hasHydrated} = useCartStore()
+  const { cart, hasHydrated } = useCartStore();
 
-  if (!hasHydrated) return null
+  if (!hasHydrated) return null;
   return (
-        <Link href="/cart" className="relative">
-            <ShoppingCart className="w-4 h-4 text-gray-600" />
-            <span className="absolute -top-3 -right-3 bg-amber-400 rounded-full w-4 h-4 flex items-center justify-center font-medium text-xs text-gray-600">
-              {cart.reduce((acc, item) => acc + item.quantity, 0)}
-            </span>
-        </Link>
-  )
-}
+    <Link href="/cart" className="relative">
+      <ShoppingCart className="w-4 h-4 text-gray-600" />
+      <span className="absolute -top-3 -right-3 bg-amber-400 rounded-full w-4 h-4 flex items-center justify-center font-medium text-xs text-gray-600">
+        {cart.reduce((acc, item) => acc + item.quantity, 0)}
+      </span>
+    </Link>
+  );
+};
 
-export default ShoppingCartIcon
+export default ShoppingCartIcon;
